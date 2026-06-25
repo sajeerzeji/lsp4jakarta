3961c5bb68e82198b3ccf4f0ded29914f4725317
Sajeer <sajeer.babu@ibm.com>
2026-06-25 12:52:00 +0530

    Added new quick-fix class RemoveConflictingSessionBeanAnnotationsQuickFix

---
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/internal/ejb/RemoveConflictingSessionBeanAnnotationsQuickFix.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+* Copyright (c) 2026 IBM Corporation and others.
+*
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License v. 2.0 which is available at
+* http://www.eclipse.org/legal/epl-2.0.
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Contributors:
+*     IBM Corporation - initial API and implementation
+*******************************************************************************/
+package org.eclipse.lsp4jakarta.jdt.internal.ejb;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.IBinding;
+import org.eclipse.lsp4j.CodeAction;
+import org.eclipse.lsp4j.Diagnostic;
+import org.eclipse.lsp4jakarta.commons.codeaction.JakartaCodeActionId;
+import org.eclipse.lsp4jakarta.jdt.core.java.codeaction.JavaCodeActionContext;
+import org.eclipse.lsp4jakarta.jdt.core.java.codeaction.RemoveAnnotationConflictQuickFix;
+
+import com.google.gson.JsonArray;
+
+/**
+ * Removes all conflicting session bean annotations except the one to keep.
+ */
+public class RemoveConflictingSessionBeanAnnotationsQuickFix extends RemoveAnnotationConflictQuickFix {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getParticipantId() {
+        return RemoveConflictingSessionBeanAnnotationsQuickFix.class.getName();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected JakartaCodeActionId getCodeActionId() {
+        return JakartaCodeActionId.EJBRemoveConflictingSessionBeanAnnotations;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<? extends CodeAction> getCodeActions(JavaCodeActionContext context, Diagnostic diagnostic,
+                                                     IProgressMonitor monitor) throws CoreException {
+        List<CodeAction> codeActions = new ArrayList<>();
+        ASTNode node = context.getCoveredNode();
+        IBinding parentType = getBinding(node);
+
+        if (parentType != null) {
+            JsonArray diagnosticData = (JsonArray) diagnostic.getData();
+            List<String> annotations = IntStream.range(0, diagnosticData.size())
+                    .mapToObj(idx -> diagnosticData.get(idx).getAsString())
+                    .collect(Collectors.toList());
+
+            for (String annotation : annotations) {
+                List<String> resultingAnnotations = new ArrayList<>(annotations);
+                resultingAnnotations.remove(annotation);
+
+                createCodeAction(diagnostic, context, parentType, codeActions,
+                                 resultingAnnotations.toArray(new String[] {}));
+            }
+        }
+
+        return codeActions;
+    }
+}
