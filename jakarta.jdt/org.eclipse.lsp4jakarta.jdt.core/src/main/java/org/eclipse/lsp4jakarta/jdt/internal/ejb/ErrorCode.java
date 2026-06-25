88407a24aebc667cfcd14fcc757942536bfc55c6
Sajeer <sajeer.babu@ibm.com>
2026-06-25 12:19:31 +0530

    Added Error Code and Diagnostic message

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/internal/ejb/ErrorCode.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/internal/ejb/ErrorCode.java
@@ -20,7 +20,8 @@ import org.eclipse.lsp4jakarta.jdt.core.java.diagnostics.IJavaErrorCode;
  */
 public enum ErrorCode implements IJavaErrorCode {
 
-    MissingPublicNoArgConstructor;
+    MissingPublicNoArgConstructor,
+    ConflictingSessionBeanAnnotations;
 
     @Override
     public String getCode() {
